cd3277762be9e5f0b447946a90812aa69fdce84f
cazicruz <dcazicruz@icloud.com>
2022-09-24 23:50:40 +0000

    task2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     from hidden_4 import *
 
-    dicovers_1 = dir(hidden_4.pyc)
+    dicovers_1 = dir(hidden_4)
 
     for s in dicovers_1[1:]:
 	print("{:s}".format(s))
