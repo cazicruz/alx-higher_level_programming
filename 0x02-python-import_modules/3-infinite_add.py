57817f98d96ca5b12d16d8e77e8d20d94a702024
cazicruz <dcazicruz@icloud.com>
2022-09-24 23:14:03 +0000

    task2

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print sum of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+
+    for i in range(count):
+	sum += sys.argv[i+1]
+
+    print("{}".format(sum))
