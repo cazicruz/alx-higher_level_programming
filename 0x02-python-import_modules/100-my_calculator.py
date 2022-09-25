21714f42b82dd862f13a6b82b9e7e94126d7bda5
cazicruz <dcazicruz@icloud.com>
2022-09-25 00:50:18 +0000

    task2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     from sys import argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         quit(1)
-
-
     a = int(argv[1])
     b = int(argv[3])
     ops = ["+", "-", "*", "/"]
-
-
+    from calculator_1 import add, sub, mul, div
     funcs = [add, sub, mul, div]
     for i, s in enumerate(ops):
         if argv[2] == s:
@@ -19,4 +15,4 @@ if __name__ == "__main__":
             break
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        quit(1i)
+        quit(1)
