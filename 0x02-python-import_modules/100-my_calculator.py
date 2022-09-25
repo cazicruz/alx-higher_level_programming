c069a5184ab2adc8114d8d0580cccee46cc155b6
cazicruz <dcazicruz@icloud.com>
2022-09-25 00:39:09 +0000

    task2

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1
+    from sys import argv
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if (len(argv) != 3):
+	print(Usage: ./100-my_calculator.py <a> <operator> <b>)
+	exit(1)
+
+    elif (argv[2] == "+"):
+	print("{} {} {} = {}".format(a, argv[2], b, add(a, b)))
+    elif (argv[2] == "-"):
+        print("{} {} {} = {}".format(a, argv[2], b, sub(a, b)))
+    elif (argv[2] == "*"):                                       
+	print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
+    elif (argv[2] == "/"):
+	print("{} {} {} = {}".format(a, argv[2], b, div(a, b)))
+    else
+	print(Unknown operator. Available operators: +, -, * and /)
+	exit(1)
