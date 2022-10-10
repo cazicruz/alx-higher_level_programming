1ce31df185aef26e877c2f79235e55ce21f6c21f
cazicruz <dcazicruz@icloud.com>
2022-10-10 16:57:18 +0000

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ''
     for ch in my_string:
-        if ch == 'c' or ch =='C':
-            my_string.remove(ch)
-    return my_string
+        if ch != 'c' and ch != 'C':
+            new_string += ch
+    return new_string
