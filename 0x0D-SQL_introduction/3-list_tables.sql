8877dcb3b29f20a3495a410465a60e08a519ff69
Onwuli <chines_davi@yahoo.com>
2022-11-30 05:17:45 -0800

    task1

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -5,5 +5,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
-
-CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256))
+SELECT TABLE IF EXISTS first_table(id INT, name VARCHAR(256))
