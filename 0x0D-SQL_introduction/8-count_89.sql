c6263e37e25bbd5c48210bff00697d6fecbe7862
Onwuli <chines_davi@yahoo.com>
2022-11-30 04:26:45 -0800

    task1

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,8 +1,7 @@
--- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
 -- New row:
 --       id = 89
---       name = Best School
 -- The database name will be passed as an argument o
 SELECT hbtn_0c_0 COUNT(*)
 FROM first_table
-WHERE id = 89;
+WHERE id = '89';
