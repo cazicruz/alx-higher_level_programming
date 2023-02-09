933a9839018bc796bc424b58cc3abd9b4849e173
Onwuli <chines_davi@yahoo.com>
2023-02-09 14:38:20 -0800

    added readme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[1], 'utf8',process.argv[3], function (error) {
+fs.writeFile(process.argv[1],process.argv[3], function (error) {
   if (error){
     console.log(error);}
 });
